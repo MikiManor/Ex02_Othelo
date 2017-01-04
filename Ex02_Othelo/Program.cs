617b403d6136cb58aa353c9666ae1714135843f7
matan_m <matan_m@MATANM-W7-PC>
2017-01-04 18:03:04 +0200

    z

--- Ex02_Othelo/Program.cs
+++ Ex02_Othelo/Program.cs
@@ -8,15 +8,24 @@ namespace Ex02_Othelo
     {
         public static void Main()
         {
-            System.Console.WriteLine("Welcome to Othelo game, have fun!");
+            Ex02.ConsoleUtils.Screen.Clear();
+            //System.Console.WriteLine("Welcome to Othelo game, have fun!\n\n"); //change "/n"
             // OtheloUI OtheloGame = new OtheloUI();
             // OtheloGame.StartPlay();
+            
             OtheloBoard board = new OtheloBoard(8);
-            // board.BoardPrint();
             
             board.BoardPrint();
+            System.Console.WriteLine("2nd round.... enter");//
+            System.Console.ReadLine();
+            Ex02.ConsoleUtils.Screen.Clear();
+            board.SetCellValue(1, 1, 2);
+            board.BoardPrint();
+
+            //int a = Random.Next(1, 11);
             System.Console.ReadLine();
             //matan miki manor
+            
         }
     }
 }
